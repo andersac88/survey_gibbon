a2cf4e455572f88ce6a9eac9c133b5bf0c416ce6
Andrew Andersen <andersac88@gmail.com>
2020-09-09 20:55:02 -0500

    initial

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+// KEY.js
+if (process.env.NODE_ENV === 'production') {
+	//we are in production - return production keys
+	module.exports = require('./prod');
+} else {
+	// we are in dev - return dev keys
+	module.exports = require('./dev');
+}
