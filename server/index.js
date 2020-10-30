e42994c4901047274a565467466c97f4a4c786c5
Andrew Andersen <andersac88@gmail.com>
2020-10-29 20:56:58 -0500

    updates

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+
 require('./models/User');
 require('./services/passport');
 
@@ -24,6 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
