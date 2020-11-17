b2cb9848af06f609f61fe99defa2066e010634e2
Andrew Andersen <andersac88@gmail.com>
2020-11-16 19:56:28 -0600

    production routing

--- server/index.js
+++ server/index.js
@@ -31,6 +31,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assests, such as, main.js file and main.css file
+	app.use(express.static('client/build'));
+	// Express will servce up index.html file if it doesn't recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function () {
