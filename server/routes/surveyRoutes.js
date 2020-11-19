5289ee803b065bddcdd25324404098db80d610b7
Andrew Andersen <andersac88@gmail.com>
2020-11-18 22:00:49 -0600

    db work

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,1 +1,6 @@
-module.exports = (app) => {};
+const requireLogin = require('../middleware/requireLogin');
+const requireCredits = require('../middleware/requireCredits');
+
+module.exports = (app) => {
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+};
