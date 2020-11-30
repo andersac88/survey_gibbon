545b4e66531d634698b1c24e6cac7fda18778a26
Andrew Andersen <andersac88@gmail.com>
2020-11-29 21:26:58 -0600

    routing

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -37,6 +37,7 @@ class Header extends Component {
 					<Link
 						to={this.props.auth ? '/surveys' : '/'}
 						className="left brand-logo"
+						style={{ paddingLeft: '15px' }}
 					>
 						Survey Gibbon
 					</Link>
--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import { withRouter } from 'react-router-dom';
 
+import * as actions from '../../actions';
 import formFields from './formFields';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
 	const reviewFields = _.map(formFields, ({ name, label }) => {
 		return (
 			<div key={name}>
@@ -27,7 +28,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 			</button>
 			<button
 				className="green white-text btn-flat right"
-				onClick={() => submitSurvey(formValues)}
+				onClick={() => submitSurvey(formValues, history)}
 			>
 				Send Survey<i className="material-icons right">email</i>
 			</button>
@@ -41,4 +42,4 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
