5ca61e0c58db5b1499b53210e747810f80037580
Andrew Andersen <andersac88@gmail.com>
2020-12-05 21:27:30 -0600

    bah

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -11,7 +11,7 @@ const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
 	app.get('/api/surveys/thanks', (req, res) => {
-		res.send('Thanks for voting');
+		res.send('Thank you for voting');
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
